80de893aedd6449375c2bdce1018f902ef374828
Accompany-Z <zwf982533@gmail.com>
2021-02-22 15:54:22 +0800

    测试

---
+++ Learn/test/src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @discription: 测试
+ * @Author: 张文峰
+ * @Date: 2021-02-22 15:44:05
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git Hub 测试");
+    }
+}
